f864ddc91bf282f3370b72405a785bd2d9a02556
Mukesh Gehlot <77880101+mukesh-gehlot@users.noreply.github.com>
2024-01-13 03:52:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let display = document.getElementById('result');
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
